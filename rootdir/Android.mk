a5d3543800dc7cd8369c7363969bc204625e487f
Aaron Kling <webgeek1234@gmail.com>
2015-05-31 16:32:27 -0500

    Add usb init rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -105,6 +105,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.nv_dev_board.usb.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.nv_dev_board.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := power.foster.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
