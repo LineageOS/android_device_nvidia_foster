0eb0c7c7303503352b605badd5fdaa5bde430954
Aaron Kling <webgeek1234@gmail.com>
2016-02-23 13:59:14 -0600

    Don't copy a file that got removed

--- device.mk
+++ device.mk
@@ -90,8 +90,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/nvaudio_conf.xml:system/etc/nvaudio_conf.xml \
-    $(LOCAL_PATH)/audio/nvaudio_fx.xml:system/etc/nvaudio_fx.xml
+    $(LOCAL_PATH)/audio/nvaudio_conf.xml:system/etc/nvaudio_conf.xml
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
