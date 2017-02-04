b4af0f9a4a03c156348c28cdf10c5daf7e8cceca
Aaron Kling <webgeek1234@gmail.com>
2017-02-04 16:20:43 -0600

    Update some permission files

--- device.mk
+++ device.mk
@@ -65,7 +65,6 @@ PRODUCT_COPY_FILES += \
 
 # NVIDIA
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/android.hardware.type.television.xml:system/etc/permissions/android.hardware.type.television.xml \
     $(LOCAL_PATH)/permissions/com.nvidia.feature.xml:system/etc/permissions/com.nvidia.feature.xml \
     $(LOCAL_PATH)/permissions/com.nvidia.feature.opengl4.xml:system/etc/permissions/com.nvidia.feature.opengl4.xml \
     $(LOCAL_PATH)/permissions/com.nvidia.nvsi.xml:system/etc/permissions/com.nvidia.nvsi.xml
