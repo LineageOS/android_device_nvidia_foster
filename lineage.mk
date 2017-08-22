874a5fbe140f9ddd473c7708fbd0fcd4a070be95
Aaron Kling <webgeek1234@gmail.com>
2017-08-22 15:03:03 -0500

    CM vendor renamed to lineage
    Change-Id: Iacd13d7461ecef4ed16195e1e6f7cf58cc1495a5

--- lineage.mk
+++ lineage.mk
@@ -3,9 +3,9 @@ $(call inherit-product, device/nvidia/foster/full_foster.mk)
 
 # Inherit some common lineage stuff.
 ifeq ($(ALTERNATE_BUILD),true)
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 else
-$(call inherit-product, vendor/cm/config/common_full_tv.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
 endif
 
 PRODUCT_NAME := lineage_foster
