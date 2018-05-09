5d65157da983e90087ab8d6817519bbdce948f79
Aaron Kling <webgeek1234@gmail.com>
2018-05-09 14:23:02 -0500

    Fix tablet alternate build
    Change-Id: Icbff68de1b67e13537209746fe6c245488a359e3

--- device_tablet.mk
+++ device_tablet.mk
@@ -2,4 +2,5 @@ PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    $(LOCAL_PATH)/permissions/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
