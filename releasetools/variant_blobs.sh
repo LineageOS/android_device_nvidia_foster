4969195eee89d368a99034d28e2d67fde03e2c2a
Aaron Kling <webgeek1234@gmail.com>
2018-06-16 16:24:56 -0500

    Update audio and media configs
    Change-Id: Iee2c2d7c6a971bee3f11b6da9b56cd370359d029

--- releasetools/variant_blobs.sh
+++ releasetools/variant_blobs.sh
@@ -35,10 +35,12 @@ elif [ "$(getprop ro.hardware)" == "darcy" ]; then
   /system/bin/toybox mv "/system/vendor/lib/liboemcrypto.darcy.so" "/system/vendor/lib/liboemcrypto.so"
   /system/bin/toybox rm -rf "/system/vendor/app/ConsoleUI"
   /system/bin/toybox sed -i 's/AUDIO_DEVICE_IN_BUILTIN_MIC|//' "/system/etc/audio_policy.conf"
+  /system/bin/toybox sed -i 's/builtin speaker" val="1"/builtin speaker" val="0"/' "/system/etc/nvaudio_conf.xml"
 else
   /system/bin/toybox rm "/system/vendor/app/eks2/eks2_darcy.dat"
   /system/bin/toybox rm "/system/vendor/lib/liboemcrypto.darcy.so"
   /system/bin/toybox rm "/system/vendor/lib/liboemcrypto.loki.so"
   /system/bin/toybox rm -rf "/system/vendor/app/ConsoleUI"
   /system/bin/toybox sed -i 's/AUDIO_DEVICE_IN_BUILTIN_MIC|//' "/system/etc/audio_policy.conf"
+  /system/bin/toybox sed -i 's/builtin speaker" val="1"/builtin speaker" val="0"/' "/system/etc/nvaudio_conf.xml"
 fi;
