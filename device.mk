75be38c3de15bd386aefc9e6b8d27c1b619d0a8d
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 22:55:23 -0600

    Inherit raydium
    Loki has a raydium touch panel
    
    Change-Id: I6a621c5b5dfcc44db9f35ac5b087a5692fcfa23e

--- device.mk
+++ device.mk
@@ -19,6 +19,7 @@ TARGET_TEGRA_CEC      ?= nvhdmi
 TARGET_TEGRA_OMX      ?= nvmm
 
 $(call inherit-product, device/nvidia/t210-common/t210.mk)
+$(call inherit-product, device/nvidia/touch/raydium.mk)
 $(call inherit-product, device/nvidia/shield-common/shield.mk)
 
 # System properties
