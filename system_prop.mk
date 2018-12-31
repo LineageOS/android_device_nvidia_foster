e9c2be70aa52afa6341a59ed578cf9415be4993c
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 23:32:27 -0600

    Add properties for usb product ids
    WIP: TODO: These are for foster_e, look into a way to dynamically
    set per model.
    
    Change-Id: I60ddb383028c68689644abbf532cd9ba40b42e0c

--- system_prop.mk
+++ system_prop.mk
@@ -8,6 +8,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.nrdp.audio.otfs=true \
     ro.nrdp.validation=ninja_6
 
+# USB Vendor and Product ID Definition
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nv.usb.vid=0955 \
+    ro.nv.usb.pid.mtp=B430 \
+    ro.nv.usb.pid.mtp.adb=B431 \
+    ro.nv.usb.pid.rndis=B434 \
+    ro.nv.usb.pid.rndis.acm.adb=af00 \
+    ro.nv.usb.pid.rndis.adb=B435 \
+    ro.nv.usb.pid.ptp=B432 \
+    ro.nv.usb.pid.ptp.adb=B433 \
+    ro.nv.usb.pid.adb=7104 \
+    ro.nv.usb.pid.accessory.adb=7105 \
+    ro.nv.usb.pid.audio_source.adb=7106 \
+    ro.nv.usb.pid.ncm=7107 \
+    ro.nv.usb.pid.ncm.adb=7108 \
+    ro.nv.usb.pid.ecm=710B \
+    ro.nv.usb.pid.ecm.adb=710C \
+    ro.nv.usb.pid.midi=7109 \
+    ro.nv.usb.pid.midi.adb=710A
+
 # WiFi
 PRODUCT_PROPERTY_OVERRIDES += \
    ap.interface=wlan0 \
