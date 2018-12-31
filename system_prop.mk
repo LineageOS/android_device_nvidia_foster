063861176148a564fd6e45a23992a50fa68c9c34
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 23:35:02 -0600

    Add configfs properties
    Change-Id: I71fa892b02f8b4bf48355b3faaaa931505d9fe12

--- system_prop.mk
+++ system_prop.mk
@@ -15,6 +15,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.nrdp.audio.otfs=true \
     ro.nrdp.validation=ninja_6
 
+# USB configfs
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.udc=700d0000.xudc \
+    sys.usb.controller=700d0000.xudc
+
 # USB Vendor and Product ID Definition
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.nv.usb.vid=0955 \
