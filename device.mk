94ce0cd2372f0c22f0ac451013ae4ceb985789c8
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 22:37:45 -0600

    Include shield-common
    This will only trigger on the primary foster builds, not
    derivatives.
    
    Change-Id: Ie5ea616749c4ea0db6bc216d451eaf7a46f24a96

--- device.mk
+++ device.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+# Only include Shield apps for first party targets
+ifneq ($(filter $(word 2,$(subst _, ,$(TARGET_PRODUCT))), foster foster_tab),)
+$(call inherit-product, device/nvidia/shield-common/shield.mk)
+endif
+
 TARGET_REFERENCE_DEVICE ?= foster
 TARGET_TEGRA_VARIANT    ?= common
 
