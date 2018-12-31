c4b9b2585bc3ad09903ebaa0f7ca3288aa685ed9
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 21:43:41 -0600

    Add charger
    Loki variant has a battery
    
    Change-Id: I890c48e45181a52788d8d9a3d9c4a7f4137b5ace

--- device.mk
+++ device.mk
@@ -114,6 +114,11 @@ PRODUCT_PACKAGES += \
     nvaudio_fx.xml
 endif
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
