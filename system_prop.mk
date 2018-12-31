5dffa047dede2782007356266ac92e753cebd2ca
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 23:35:02 -0600

    Add configfs properties
    Change-Id: Ia0f8c5e45bb2c6006859a6edb4a47d5cb54a8b3d

--- system_prop.mk
+++ system_prop.mk
@@ -3,3 +3,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.nrdp.modelgroup=SHIELDANDROIDTV \
     ro.vendor.nrdp.audio.otfs=true \
     ro.vendor.nrdp.validation=ninja_6
+
+# USB configfs
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.sys.usb.udc=700d0000.xudc \
+    sys.usb.controller=700d0000.xudc
