20cc6f996391f5410da4ef5f4aa74459e8c99ce8
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 21:43:41 -0600

    Add charger
    Loki variant has a battery
    
    Change-Id: I7ee2b179bfa2b0ed21414979fe314b6f1a3b4f63

--- device.mk
+++ device.mk
@@ -92,6 +92,11 @@ PRODUCT_PACKAGES += \
     nvaudio_fx.xml
 endif
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
 # Fingerprint override
 BUILD_FINGERPRINT := NVIDIA/foster_e/foster:8.0.0/OPR6.170623.010/3507953_1441.7411:user/release-keys
 
