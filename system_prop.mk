a3d4b9be6b6254c0e2d89247c80db54a14cefa9c
Aaron Kling <webgeek1234@gmail.com>
2018-12-30 23:51:40 -0600

    Add a gamestream optimization property
    Change-Id: I8dc2f9bbae1afc4937da2a098c2e43e853b212d4

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bt.bdaddr_path=/mnt/vendor/factory/bluetooth/bt_mac.txt
 
+# Gamestreaming specific properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.gamestream.display.optimize=1
+
 # NRDP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.nrdp.modelgroup=SHIELDANDROIDTV \
