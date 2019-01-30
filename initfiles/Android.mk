a32162a05e8f47f3f81a058d5f3fd92faafddbb9
Aaron Kling <webgeek1234@gmail.com>
2019-01-30 15:39:59 -0600

    Add loki_e support
    This covers loki_e_base, loki_e_lte, and loki_e_wifi
    
    Change-Id: I2427d1c7176ce0d64ada9459c91d6d3115379d39

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -57,6 +57,27 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE        := fstab.loki_e_base
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := fstab.foster_e
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := fstab.loki_e_lte
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := fstab.foster_e
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := fstab.loki_e_wifi
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := fstab.foster_e
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE        := fstab.porg
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := fstab.foster_e
@@ -113,6 +134,30 @@ include $(CLEAR_VARS)
 LOCAL_MODULE               := init.jetson_cv.rc
 LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.jetson_cv.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.loki_e_base.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.loki_e_base.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.loki_e_lte.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.loki_e_lte.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.loki_e_wifi.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.loki_e_wifi.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
@@ -177,6 +222,27 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.loki_e_base.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.loki.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.loki_e_lte.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.loki.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.loki_e_wifi.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.loki.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.porg.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.recovery.foster.rc
@@ -201,6 +267,14 @@ include $(CLEAR_VARS)
 LOCAL_MODULE               := init.foster_e_common.rc
 LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.foster_e_common.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.loki_e_common.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.loki_e_common.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
@@ -246,6 +320,27 @@ LOCAL_MODULE       := power.jetson_e.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_ODM_MODULE   := true
 LOCAL_SRC_FILES    := power.foster_e_common.rc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := power.loki_e_base.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_ODM_MODULE   := true
+LOCAL_SRC_FILES    := power.loki_e_common.rc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := power.loki_e_lte.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_ODM_MODULE   := true
+LOCAL_SRC_FILES    := power.loki_e_common.rc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := power.loki_e_wifi.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_ODM_MODULE   := true
+LOCAL_SRC_FILES    := power.loki_e_common.rc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
