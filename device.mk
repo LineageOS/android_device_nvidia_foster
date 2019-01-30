19c2d49214208685f02219073981474bdb5dd0b9
Aaron Kling <webgeek1234@gmail.com>
2019-01-30 15:39:59 -0600

    Add loki_e support
    This covers loki_e_base, loki_e_lte, and loki_e_wifi
    
    Change-Id: Id255de9e87c82cc99b0251dcad657e2ee70b8b66

--- device.mk
+++ device.mk
@@ -32,24 +32,37 @@ PRODUCT_PACKAGES += \
     fstab.foster_e_hdd \
     fstab.jetson_cv \
     fstab.jetson_e \
+    fstab.loki_e_base \
+    fstab.loki_e_lte \
+    fstab.loki_e_wifi \
     fstab.sif \
     init.darcy.rc \
     init.foster_e.rc \
     init.foster_e_hdd.rc \
     init.foster_e_common.rc \
+    init.loki_e_common.rc \
     init.loki_foster_e_common.rc \
     init.jetson_cv.rc \
     init.jetson_e.rc \
+    init.loki_e_base.rc \
+    init.loki_e_lte.rc \
+    init.loki_e_wifi.rc \
     init.sif.rc \
     init.recovery.darcy.rc \
     init.recovery.foster_e.rc \
     init.recovery.foster_e_hdd.rc \
     init.recovery.jetson_cv.rc \
     init.recovery.jetson_e.rc \
+    init.recovery.loki_e_base.rc \
+    init.recovery.loki_e_lte.rc \
+    init.recovery.loki_e_wifi.rc \
     init.recovery.sif.rc \
     power.darcy.rc \
     power.foster_e.rc \
     power.foster_e_hdd.rc \
     power.jetson_cv.rc \
     power.jetson_e.rc \
+    power.loki_e_base.rc \
+    power.loki_e_lte.rc \
+    power.loki_e_wifi.rc \
     power.sif.rc
