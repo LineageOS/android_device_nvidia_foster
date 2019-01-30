ed1d8fe35d55d8821289ead1192b626791452c44
Aaron Kling <webgeek1234@gmail.com>
2019-01-30 14:29:38 -0600

    Add jetson tx1 support
    This covers jetson_e and jetson_cv
    
    Change-Id: I596dc4b151bd3387bb92ca65770f0ab49f312370

--- device.mk
+++ device.mk
@@ -35,18 +35,26 @@ PRODUCT_PACKAGES += \
     fstab.foster \
     fstab.foster_e \
     fstab.foster_e_hdd \
+    fstab.jetson_cv \
+    fstab.jetson_e \
     fstab.sif \
     init.darcy.rc \
     init.foster_e.rc \
     init.foster_e_hdd.rc \
     init.foster_e_common.rc \
     init.loki_foster_e_common.rc \
+    init.jetson_cv.rc \
+    init.jetson_e.rc \
     init.sif.rc \
     init.recovery.darcy.rc \
     init.recovery.foster_e.rc \
     init.recovery.foster_e_hdd.rc \
+    init.recovery.jetson_cv.rc \
+    init.recovery.jetson_e.rc \
     init.recovery.sif.rc \
     power.darcy.rc \
     power.foster_e.rc \
     power.foster_e_hdd.rc \
+    power.jetson_cv.rc \
+    power.jetson_e.rc \
     power.sif.rc
