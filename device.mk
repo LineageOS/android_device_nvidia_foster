e194eb35a2c3c4b7638758845a3c76dc493267f3
Aaron Kling <webgeek1234@gmail.com>
2019-03-02 16:05:20 -0600

    Add lkm scripts
    Change-Id: I015bd609169bb49b1216669a6d14fedc744442c2

--- device.mk
+++ device.mk
@@ -138,6 +138,11 @@ PRODUCT_PACKAGES += \
     gps.conf \
     gpsconfig.xml
 
+# Loadable kernel modules
+PRODUCT_PACKAGES += \
+    init.lkm.rc \
+    lkm_loader
+
 # Media config
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
