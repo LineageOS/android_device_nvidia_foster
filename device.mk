da1c4aaf01d319ef36a0cb9837df26a62e9a6f75
Aaron Kling <webgeek1234@gmail.com>
2019-03-02 16:05:20 -0600

    Import LKM scripts
    From stock 8.2.0.
    
    Change-Id: I0fcebfd89c2d000e2a3843998e9327b6e6fd9011

--- device.mk
+++ device.mk
@@ -146,6 +146,11 @@ PRODUCT_PACKAGES += \
     cypress-fmac
 endif
 
+# Loadable kernel modules
+PRODUCT_PACKAGES += \
+    init.lkm.rc \
+    lkm_loader
+
 # Media config
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_ODM)/etc/media_codecs_google_audio.xml \
