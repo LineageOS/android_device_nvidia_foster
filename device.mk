9a3bf284e70436f67e15435fc50b528471cd9ef2
Aaron Kling <webgeek1234@gmail.com>
2019-05-06 09:31:12 -0500

    Add nvphs hal
    Change-Id: I0c821b50d19a81fca6afaf609d067a227caff126

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 TARGET_TEGRA_AUDIO ?= nvaudio
 TARGET_TEGRA_CEC   ?= nvhdmi
+TARGET_TEGRA_PHS   ?= nvphs
 
 $(call inherit-product, device/nvidia/t210-common/t210.mk)
 $(call inherit-product, device/nvidia/touch/raydium.mk)
@@ -172,6 +173,12 @@ PRODUCT_COPY_FILES += \
 # NVIDIA specific permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/com.nvidia.feature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nvidia.feature.xml
+
+# PHS
+ifeq ($(TARGET_TEGRA_PHS),nvphs)
+PRODUCT_PACKAGES += \
+    nvphsd.conf
+endif
 
 # Power
 TARGET_POWERHAL_VARIANT := tegra
