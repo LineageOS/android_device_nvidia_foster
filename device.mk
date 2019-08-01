bfb2b257d055a368f7248ff603003f380155c556
Aaron Kling <webgeek1234@gmail.com>
2019-08-01 15:09:03 -0500

    Add foster vendor model properties
    Change-Id: I2e48e94ee4e8b805b30b2489c893a83d46c1b4c6

--- device.mk
+++ device.mk
@@ -24,6 +24,11 @@ TARGET_SCREEN_WIDTH      := 1080
 
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
+PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
+    ro.product.vendor.device \
+    ro.product.vendor.model \
+    ro.product.vendor.name
+
 # Init related
 PRODUCT_PACKAGES += \
     fstab.foster \
