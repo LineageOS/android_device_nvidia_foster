c81d50792c0ef0d08cd8ba7cd496518e299f8ef4
Billy Laws <blaws05@gmail.com>
2021-03-25 22:43:14 -0500

    Update dtb building for Q
    Change-Id: I7b3f7115da5d8b2d1a2f1299decd7df880463d3e

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -20,7 +20,7 @@ INSTALLED_DTBIMAGE_TARGET_mdarcy := $(PRODUCT_OUT)/install/dtb_mdarcy.img
 $(INSTALLED_DTBIMAGE_TARGET_mdarcy): $(INSTALLED_KERNEL_TARGET) | mkdtimg
 	echo -e ${CL_GRN}"Building mdarcy DTImage"${CL_RST}
 	@mkdir -p $(PRODUCT_OUT)/install
-	mkdtimg create $@ --id=2894 \
+	$(HOST_OUT_EXECUTABLES)/mkdtimg create $@ --id=2894 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-darcy-p2894-0050-a08-00.dtb --rev=0x0a8 --custom0=0x28 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-darcy-p2894-2551-b00-00.dtb --rev=0xb00 --custom0=2551 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-darcy-p2894-3551-b03-00.dtb --rev=0xb03 --custom0=3551
@@ -29,7 +29,7 @@ INSTALLED_DTBIMAGE_TARGET_sif    := $(PRODUCT_OUT)/install/dtb_sif.img
 $(INSTALLED_DTBIMAGE_TARGET_sif): $(INSTALLED_KERNEL_TARGET) | mkdtimg
 	echo -e ${CL_GRN}"Building sif DTImage"${CL_RST}
 	@mkdir -p $(PRODUCT_OUT)/install
-	mkdtimg create $@ --id=3425 --custom0=0x140 \
+	$(HOST_OUT_EXECUTABLES)/mkdtimg create $@ --id=3425 --custom0=0x140 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-sif-p3425-0500-a01.dtb --rev=0xa1 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-sif-p3425-0500-a02.dtb --rev=0xa2 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-sif-p3425-0500-a04.dtb --rev=0xa3 \
@@ -62,7 +62,7 @@ $(INSTALLED_DTB_TARGETS): $(INSTALLED_KERNEL_TARGET) | $(ACP)
 	@mkdir -p $(PRODUCT_OUT)/install
 	cp $(@F:%=$(KERNEL_OUT)/arch/arm64/boot/dts/%) $(PRODUCT_OUT)/install/
 
-ALL_PREBUILT += $(INSTALLED_DTBIMAGE_TARGET_mdarcy) $(INSTALLED_DTBIMAGE_TARGET_sif) $(INSTALLED_DTB_TARGETS)
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTBIMAGE_TARGET_mdarcy) $(INSTALLED_DTBIMAGE_TARGET_sif) $(INSTALLED_DTB_TARGETS)
 
 .PHONY: dtbimage
 dtbimage: $(INSTALLED_DTBIMAGE_TARGET_mdarcy) $(INSTALLED_DTBIMAGE_TARGET_sif)
