93a508729e17487f59e24711f30350e481b171bb
Aaron Kling <webgeek1234@gmail.com>
2021-09-14 14:53:07 -0500

    Use a recovery specific kernel defconfig
    The kernel build scripts don't support a recovery specific variant
    defconfig, so this needs to be a full defconfig
    
    Change-Id: I52d4b423e782894effb619020995a4c2b46ef4f2

--- twrp/twrp.mk
+++ twrp/twrp.mk
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_KERNEL_ADDITIONAL_CONFIG := tegra_android_recovery_defconfig
-BOARD_KERNEL_CMDLINE            := androidboot.selinux=permissive
-LZMA_RAMDISK_TARGETS            := recovery
+TARGET_KERNEL_CONFIG := tegra_android_recovery_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+LZMA_RAMDISK_TARGETS := recovery
 
 PRODUCT_COPY_FILES += \
     device/nvidia/foster/twrp/twrp.fstab.emmc:recovery/root/etc/twrp.fstab.emmc \
