7af376b654b6fe854e47d0f01828b01d88258bf2
Aaron Kling <webgeek1234@gmail.com>
2021-12-14 17:37:52 -0600

    Update audio and media configs for rel-shield-r
    Involves splitting the audio policy for atv and mobile targets
    
    Change-Id: If05aa56e60fd33831768cbe89cd3280895325bd7

--- media/Android.mk
+++ media/Android.mk
@@ -26,10 +26,18 @@ include $(CLEAR_VARS)
 LOCAL_MODULE        := audio_policy_configuration.xml
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
+ifeq ($(PRODUCT_IS_ATV),true)
+ifeq ($(TARGET_TEGRA_DOLBY),true)
+LOCAL_SRC_FILES     := audio_policy_configuration_nv_tv.xml
+else
+LOCAL_SRC_FILES     := audio_policy_configuration_nv_tv_nodolby.xml
+endif
+else
 ifeq ($(TARGET_TEGRA_DOLBY),true)
 LOCAL_SRC_FILES     := audio_policy_configuration_nv.xml
 else
 LOCAL_SRC_FILES     := audio_policy_configuration_nv_nodolby.xml
+endif
 endif
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
