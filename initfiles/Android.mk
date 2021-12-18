245ae2a27450352630c57c0dd91ec2fb7db9a12b
Aaron Kling <webgeek1234@gmail.com>
2021-12-17 19:32:46 -0600

    Disable usb in recovery for sif
    It's causing a kernel watchdog panic after 2 minutes
    
    Change-Id: I7a4ef4756ebe355c2f6b32409763560d069a2415

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -355,7 +355,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.sif.rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.recovery.foster.rc
+LOCAL_SRC_FILES    := init.recovery.sif.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
