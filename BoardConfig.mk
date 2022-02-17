449384459048430d66e5553095166b47879504f3
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-17 15:06:15 -0500

    Remove deprecated BOARD_SUPPRESS_EMMC_WIPE flag
    * No longer supported past `lineage-16.0`.
    
    Change-Id: I7bfa5ae25ba3accc20abd7360d5953b5fb1ab8b3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,6 @@ BOARD_KERNEL_IMAGE_NAME       := Image.gz
 
 # Recovery
 TARGET_RECOVERY_FSTAB        := device/nvidia/foster/initfiles/fstab.foster
-BOARD_SUPPRESS_EMMC_WIPE     := true
 TARGET_RECOVERY_UPDATER_LIBS := librecoveryupdater_tegra
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 
