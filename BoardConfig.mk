0631cc6952bedda88b5d7f07fe599f441d403c7a
Aaron Kling <webgeek1234@gmail.com>
2022-03-15 21:13:40 -0500

    Use gcc for kernel build
    Lineage has switched to clang for kernel builds by default. The
    nvidia 4.9 kernel does not build with clang.
    
    Change-Id: Ie00632002a532a7a090d50c1951575c361244f7d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,7 @@ WITH_LINEAGE_CHARGER := false
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(dir $(TARGET_PREBUILT_KERNEL))/*.ko)
 endif
+TARGET_KERNEL_CLANG_COMPILE    := false
 TARGET_KERNEL_SOURCE           := kernel/nvidia/kernel-$(TARGET_TEGRA_KERNEL)
 TARGET_KERNEL_CONFIG           := tegra_android_defconfig
 TARGET_KERNEL_RECOVERY_CONFIG  := tegra_android_recovery_defconfig
