625c32ebc023d866a31da2574577b3f9a594b044
Aaron Kling <webgeek1234@gmail.com>
2022-12-25 01:01:15 -0600

    Build nx dtimage
    Change-Id: I8c731ce0d08f2e40d98691400d68e5cef6ffb41b

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -61,7 +61,17 @@ $(INSTALLED_DTBIMAGE_TARGET_sif): $(INSTALLED_KERNEL_TARGET) | mkdtimg
 		$(KERNEL_OUT)/arch/arm64/boot/dts/$(DTB_SUBFOLDER)tegra210b01-sif-p3425-0500-a04.dtb --rev=0xa3 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/$(DTB_SUBFOLDER)tegra210b01-sif-p3425-0500-a04.dtb --rev=0xa4
 
-ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTBIMAGE_TARGET_mdarcy) $(INSTALLED_DTBIMAGE_TARGET_sif)
+INSTALLED_DTBIMAGE_TARGET_nx     := $(PRODUCT_OUT)/install/nx.dtb.img
+$(INSTALLED_DTBIMAGE_TARGET_nx): $(INSTALLED_KERNEL_TARGET) | mkdtimg
+	echo -e ${CL_GRN}"Building nx DTImage"${CL_RST}
+	@mkdir -p $(PRODUCT_OUT)/install
+	$(HOST_OUT_EXECUTABLES)/mkdtimg create $@ \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/$(DTB_SUBFOLDER)tegra210-odin.dtb    --id=0x4F44494E \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/$(DTB_SUBFOLDER)tegra210b01-odin.dtb --id=0x4F44494E --rev=0xb01 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/$(DTB_SUBFOLDER)tegra210b01-vali.dtb --id=0x56414C49 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/$(DTB_SUBFOLDER)tegra210b01-frig.dtb --id=0x46524947
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTBIMAGE_TARGET_mdarcy) $(INSTALLED_DTBIMAGE_TARGET_sif) $(INSTALLED_DTBIMAGE_TARGET_nx)
 endif
 
 EKS_DAT_SYMLINK := $(TARGET_OUT_VENDOR)/app/eks2/eks2.dat
