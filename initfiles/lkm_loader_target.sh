9639482e20ac6154aaeeb104e0bea07ffbf61dab
Aaron Kling <webgeek1234@gmail.com>
2023-01-11 01:09:00 -0600

    lkm: Use insmod for cypress_fmac
    Since this is built out of tree, it is not included in modules.dep,
    thus modprobe does not work properly.
    
    Change-Id: I3071e641040d777f82f6f42f136540e0498b33a1

--- initfiles/lkm_loader_target.sh
+++ initfiles/lkm_loader_target.sh
@@ -2,7 +2,10 @@
 
 hardwareName=$(getprop ro.hardware)
 if [ "`cat /proc/device-tree/brcmfmac_pcie_wlan/status`" = "okay" ]; then
-  /vendor/bin/modprobe -a -d /vendor/lib/modules brcmfmac;
+  /vendor/bin/insmod /vendor/lib/modules/compat.ko
+  /vendor/bin/insmod /vendor/lib/modules/cy_cfg80211.ko
+  /vendor/bin/insmod /vendor/lib/modules/brcmutil.ko
+  /vendor/bin/insmod /vendor/lib/modules/brcmfmac.ko
 elif [ "`cat /proc/device-tree/bcmdhd_wlan/status`" = "okay" ]; then
   /vendor/bin/modprobe -a -d /vendor/lib/modules bcmdhd;
 fi
