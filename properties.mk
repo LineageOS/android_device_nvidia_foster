d054addf07fccf2811449e5f3668823fc69ed3c0
Nolen Johnson <johnsonnolen@gmail.com>
2023-12-02 00:32:36 -0500

    Move bluetooth name to bluetooth.prop
    * Sadly in PRODUCT_PROPERTY_OVERRIDES this
      translates to 3 seperate props, example:
    
    ```
    bluetooth.device.default_name=SHIELD
    Android
    TV
    ```
    
    * The build system flips out when quotes are
      used.
    
    * Can't move whole device to *.prop because we
      ifdef so many of these in tegra trees that it
      would be disgusting.
    
    Change-Id: I4c611c64652d28f65505382db7b3e08bc6f3fabf

--- properties.mk
+++ properties.mk
@@ -2,10 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.media.avsync=true
 
-# Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
-    bluetooth.device.default_name=SHIELD Android TV
-
 ifneq ($(TARGET_TEGRA_KERNEL),3.10)
 # Bpf
 PRODUCT_PROPERTY_OVERRIDES += \
