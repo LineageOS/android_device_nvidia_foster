832d5c4913b966667e467a79f6a7e62b27f7674d
Aaron Kling <webgeek1234@gmail.com>
2024-01-06 02:24:34 -0600

    Refactor audio policy
    Previously, audio policy was handled by product type, such as tablet or
    atv. This was never correct, as policy should be based on the device
    variant and is not affect by product type. This sets the default policy
    to the basic set top box and devkit variant, and moves the mobile
    variant to be used by devices with speakers. As the variants are no longer
    in the same folder as other xmls, all includes need to use absolute paths.
    
    Change-Id: I9de7848e653cf90353be915aec46247fa9d49739

--- media/Android.mk
+++ media/Android.mk
@@ -26,12 +26,38 @@ include $(CLEAR_VARS)
 LOCAL_MODULE        := audio_policy_configuration.xml
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
-ifeq ($(PRODUCT_IS_ATV),true)
-LOCAL_SRC_FILES     := audio_policy_configuration_nv_tv.xml
-else
-LOCAL_SRC_FILES     := audio_policy_configuration_nv.xml
-endif
+LOCAL_SRC_FILES     := audio_policy_configuration.xml
 LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := audio_policy_configuration_dragon.xml
+LOCAL_MODULE_STEM          := audio_policy_configuration.xml
+LOCAL_MODULE_TAGS          := optional
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := audio_policy_configuration_loki.xml
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := audio/sku_dragon
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := audio_policy_configuration_loki.xml
+LOCAL_MODULE_STEM          := audio_policy_configuration.xml
+LOCAL_MODULE_TAGS          := optional
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := audio_policy_configuration_loki.xml
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := audio/sku_loki
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := audio_policy_configuration_nx.xml
+LOCAL_MODULE_STEM          := audio_policy_configuration.xml
+LOCAL_MODULE_TAGS          := optional
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := audio_policy_configuration_nx.xml
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := audio/sku_nx
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
