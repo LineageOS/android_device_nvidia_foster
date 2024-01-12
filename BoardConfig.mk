cf95e39b5d003c90205be4d8c291d5e90052a90f
Aaron Kling <webgeek1234@gmail.com>
2024-01-11 23:01:11 -0600

    Build nvgpu as an oot module
    Change-Id: I6f593277e74d361d833214563141fe44fb0b7c36

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,10 @@ TARGET_KERNEL_CONFIG           := tegra_android_defconfig
 BOARD_KERNEL_IMAGE_NAME        := Image.gz
 TARGET_KERNEL_ADDITIONAL_FLAGS := "NV_BUILD_KERNEL_OPTIONS=$(TARGET_TEGRA_KERNEL)"
 BOARD_KERNEL_CMDLINE           := "androidboot.boot_devices=sdhci-tegra.0,tegra-sata.0,sdhci-tegra.3"
+
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/nvidia
+TARGET_KERNEL_EXT_MODULES := \
+    nvgpu/drivers/gpu/nvgpu:kbuild
 include device/nvidia/foster/modules.mk
 
 # Recovery
