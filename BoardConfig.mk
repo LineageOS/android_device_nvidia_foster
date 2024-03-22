36b932a37f72fb739a31be61988edcada3f05f6b
Aaron Kling <webgeek1234@gmail.com>
2024-03-21 22:51:09 -0500

    Add default fingerprint
    As of Android 14 QPR2, foster Lineage release key fingerprints are
    longer than 92 characters. This cause crashes on prop read. So,
    hardcode a default that is shorter than 92 characters, so it can be
    overridden later.
    
    Change-Id: I47abbac4344252262c47b865fbda871559fcbf30

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,9 @@ endif
 # Charger
 WITH_LINEAGE_CHARGER := false
 
+# Fingerprint override
+BUILD_FINGERPRINT := NVIDIA/foster_e/foster:11/RQ1A.210105.003/7825230_3167.5736:user/release-keys
+
 # Kernel
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(dir $(TARGET_PREBUILT_KERNEL))/*.ko)
