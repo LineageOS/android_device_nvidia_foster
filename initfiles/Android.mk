LineageOS/android_device_nvidia_t194-common

Don't use post install cmd for symlinks
Deterministic partition images doesn't work right with them

Change-Id: I018d744352e882b3603f58bf3c2aa24742238f27

vendor/r35/firmware/Android.mk
@@ -23,8 +23,7 @@ LOCAL_MODULE_CLASS         := ETC
 LOCAL_MODULE_PATH          := $(TARGET_OUT_VENDOR)/firmware/nvidia/tegra194
 LOCAL_MODULE_TAGS          := optional
 LOCAL_MODULE_OWNER         := nvidia
-LOCAL_POST_INSTALL_CMD     := \
-    ln -sf ./nvidia/tegra194/xusb.bin $(TARGET_OUT_VENDOR)/firmware/tegra19x_xusb_firmware
+LOCAL_REQUIRED_MODULES     := tegra19x_xusb_firmware
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

LineageOS/android_device_nvidia_foster

initfiles/Android.mk
@@ -120,7 +120,5 @@ LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.eks2.rc
 LOCAL_VENDOR_MODULE        := true
 LOCAL_MODULE_RELATIVE_PATH := init
-LOCAL_POST_INSTALL_CMD     := \
-    mkdir -p $(TARGET_OUT_VENDOR)/app/eks2; \
-    ln -sf /data/vendor/eks2/eks2.dat $(TARGET_OUT_VENDOR)/app/eks2/eks2.dat
+LOCAL_REQUIRED_MODULES     := eks2_symlink
 include $(BUILD_PREBUILT)