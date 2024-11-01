9d4391824ec9f804b661ea5a5c645e65844c6780
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-01 10:25:47 -0400

    dnm baracus
    Change-Id: If2e28d94419fed16e37eebebce3cf7e1af7d5c5d

--- device.mk
+++ device.mk
@@ -163,8 +163,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_FORCE_PREBUILT_KERNEL := true
 else ifeq ($(TARGET_TEGRA_KERNEL),4.9)
-PRODUCT_PACKAGES += \
-    cypress-fmac
+#PRODUCT_PACKAGES += \
+#    cypress-fmac
 endif
 
 # Keylayouts
