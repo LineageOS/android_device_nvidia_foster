2c6367bac8143bd58b8e02ea01bef0ed49e2956f
Aaron Kling <webgeek1234@gmail.com>
2025-04-22 19:55:53 -0500

    Load fan driver in recovery
    We really should let the fan fire up in recovery.
    
    Change-Id: I913d44f2f222d10fc0ef120972f87482562b7582

--- modules.mk
+++ modules.mk
@@ -113,7 +113,9 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := \
 BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := \
     exfat \
     hid-nvidia-blake \
-    hid-jarvis-remote
+    hid-jarvis-remote \
+    pwm_fan \
+    therm_fan_est
 
 BOOT_KERNEL_MODULES     := $(addsuffix .ko,$(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD))
 RECOVERY_KERNEL_MODULES := $(addsuffix .ko,$(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD))
