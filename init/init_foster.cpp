1c36ec33be85e551a290ecf1a5a362300d4a1b97
Aaron Kling <webgeek1234@gmail.com>
2025-05-07 19:41:31 -0500

    Update fingerprints to match stock 9.2.1
    As of 9.2.0, the fingerprints for all shield devices split completely,
    but we missed that. 9.2.1 continues this trend. The default fp for
    devkits is now based on mdarcy.
    
    Change-Id: I247a807e1e55831b16c8e4e08e15ffe12b0555a3

--- init/init_foster.cpp
+++ init/init_foster.cpp
@@ -175,12 +175,18 @@ void vendor_load_properties()
 		{ "baracus","baracus",      "baracus",      "Jamboard",          2180, 9999, 23, 320 },
 		{ "dragon", "dragon",       "dragon",       "Pixel C",              3,    0, 23, 320 }
 	};
-	tegra_init::build_version tav = { "11", "RQ1A.210105.003", "13961456_3871.0251" };
+	tegra_init::build_version tav = { "11", "RQ1A.210105.003", "7825230_4040.2147" };
 
 	tegra_init ti(devices);
 
-	if (ti.is_model("sif") || ti.is_model("mdarcy")) {
-		tav = { "11", "RQ1A.210105.003", "7825230_3871.0251" };
+	if (ti.is_model("foster_e")) {
+		tav = { "11", "RQ1A.210105.003", "13961456_4040.2147" };
+	} else if (ti.is_model("foster_e_hdd")) {
+		tav = { "11", "RQ1A.210105.003", "13961457_4040.2147" };
+	} else if (ti.is_model("darcy")) {
+		tav = { "11", "RQ1A.210105.003", "13961458_4040.2147" };
+	} else if (ti.is_model("sif")) {
+		tav = { "11", "RQ1A.210105.003", "7825199_4040.2147" };
 	} else if (ti.is_model("dragon")) {
 		tav = { "8.1.0", "OPM8.190605.005", "5749003" };
 
