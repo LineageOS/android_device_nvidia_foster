b20dfae465fdc712570421a1a169659040fee8a8
Nolen Johnson <johnsonnolen@gmail.com>
2025-08-20 15:59:19 -0400

    init: Add baracus information
    Change-Id: Iad6ac2d8595addfef50304c99e3a2b0a19216df1

--- init/init_foster.cpp
+++ init/init_foster.cpp
@@ -126,6 +126,8 @@ void vendor_set_sku(tegra_init *ti)
 		ti->property_set("ro.boot.product.vendor.sku", "nx");
 	else if (ti->is_model("dragon"))
 		ti->property_set("ro.boot.product.vendor.sku", "dragon");
+        else if (ti->is_model("baracus"))
+                ti->property_set("ro.boot.product.vendor.sku", "baracus");
 }
 
 void vendor_set_nrdp_props(tegra_init *ti)
@@ -187,6 +189,8 @@ void vendor_load_properties()
 		tav = { "11", "RQ1A.210105.003", "13961458_4040.2147" };
 	} else if (ti.is_model("sif")) {
 		tav = { "11", "RQ1A.210105.003", "7825199_4040.2147" };
+        } else if (ti.is_model("baracus")) {
+                tav = { "7.0", "NRT1.240624.004", "12020658" };
 	} else if (ti.is_model("dragon")) {
 		tav = { "8.1.0", "OPM8.190605.005", "5749003" };
 
