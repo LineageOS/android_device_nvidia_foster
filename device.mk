8d6fad0fe43d8c82c2188ccd1a8722883bc66c2c
Aaron Kling <webgeek1234@gmail.com>
2025-10-22 13:03:06 -0500

    Move tv input build rules to tegra-common
    Change-Id: Ibc08f66d3dcbe5db5ccb8ac8e37a7341a16a8e45

--- device.mk
+++ device.mk
@@ -104,12 +104,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
 
-# ATV specific stuff
-ifeq ($(PRODUCT_IS_ATV),true)
-    PRODUCT_PACKAGES += \
-        android.hardware.tv.input@1.0-impl
-endif
-
 # Audio
 ifneq ($(TARGET_TEGRA_AUDIO),)
 PRODUCT_PACKAGES += \
