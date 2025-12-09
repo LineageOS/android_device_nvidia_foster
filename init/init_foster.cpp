e10e2ba14b518ef2251dd31bc620d16b54a59135
Aaron Kling <webgeek1234@gmail.com>
2025-12-09 00:03:56 -0600

    Update fingerprints to match stock 9.2.2
    Change-Id: I439cefcd25d3a00c0bec51330f180ac61fdce8db

--- init/init_foster.cpp
+++ init/init_foster.cpp
@@ -184,18 +184,18 @@ void vendor_load_properties()
 		{ "baracus","baracus",      "baracus",      "Jamboard",          2180, 9999, 23, 320 },
 		{ "dragon", "dragon",       "dragon",       "Pixel C",              3,    0, 23, 320 }
 	};
-	tegra_init::build_version tav = { "11", "RQ1A.210105.003", "7825230_4040.2147" };
+	tegra_init::build_version tav = { "11", "RQ1A.210105.003", "7825230_4231.0265" };
 
 	tegra_init ti(devices);
 
 	if (ti.is_model("foster_e")) {
-		tav = { "11", "RQ1A.210105.003", "13961456_4040.2147" };
+		tav = { "11", "RQ1A.210105.003", "13961456_4231.0265" };
 	} else if (ti.is_model("foster_e_hdd")) {
-		tav = { "11", "RQ1A.210105.003", "13961457_4040.2147" };
+		tav = { "11", "RQ1A.210105.003", "13961457_4231.0265" };
 	} else if (ti.is_model("darcy")) {
-		tav = { "11", "RQ1A.210105.003", "13961458_4040.2147" };
+		tav = { "11", "RQ1A.210105.003", "13961458_4231.0265" };
 	} else if (ti.is_model("sif")) {
-		tav = { "11", "RQ1A.210105.003", "7825199_4040.2147" };
+		tav = { "11", "RQ1A.210105.003", "7825199_4231.0265" };
 	} else if (ti.is_model("dragon")) {
 		tav = { "8.1.0", "OPM8.190605.005", "5749003" };
 
